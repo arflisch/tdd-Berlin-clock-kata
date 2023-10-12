94fa4ab576952ec53e256bff0e5007d066abd2a9
arnaud-flisch-vinci <arnaud.flisch@student.vinci.be>
2023-10-12 18:18:23 +0200

    ligneTtesEnBas update

--- devops/app.js
+++ devops/app.js
@@ -1,22 +1,9 @@
 export class Main{
     ligneToutesEnBas(number) {
-        let nombreMin
-        if (number % 5 === 0){
-            nombreMin = number/5;
-            return "Aucune lampe allumé";
-        }else{
-            if ( number % 5 ===1){
-                return "Lampe 1 en bas jaune allumé";
-            };
-            if (number % 5 === 2){
-                return "Lampe 1 et 2 en bas jaune allumé";
-            }
-            if (number % 5 === 3){
-                return "Lampe 1, 2 et 3 en bas jaune allumé";
-            }
-            if (number % 5 === 4){
-                return "Les 4 lampes en bas jaune allumé";
-            }
-        };
+        if(number%5 === 0) return 0;
+        if(number%5 === 1) return 1;
+        if(number%5 === 2) return 2;
+        if(number%5 === 3) return 3;
+        if(number%5 === 4) return 4;
     }
 }
--- devops/spec/appspec.js
+++ devops/spec/appspec.js
@@ -2,18 +2,41 @@ import { Main } from "../app.js";
 
 describe("Ligne simple min", function() {
     let main = new Main();
-    let dateMinute = new Date().getMinutes;
+    //let dateMinute = new Date().getMinutes;
     it("doit return 1 quand je donne 1", function(){
         
+        let result = main.ligneToutesEnBas(5);
 
-        let result = main.ligneToutesEnBas(dateMinute);
-
-        expect(result).toBe("Lampe 1 en bas jaune allumé");
+        expect(result).toBe(0);
     })
-    it("doit return 2 quand je donne 2", function(){
+    it("doit return 1 quand je donne 1", function(){
+        
+        let result = main.ligneToutesEnBas(1);
+
+        expect(result).toBe(1);
+    })
+    it("doit return 1 quand je donne 1", function(){
+        
+        let result = main.ligneToutesEnBas(2);
+
+        expect(result).toBe(2);
+    })
+    it("doit return 1 quand je donne 1", function(){
+        
+        let result = main.ligneToutesEnBas(3);
+
+        expect(result).toBe(3);
+    })
+    it("doit return 1 quand je donne 1", function(){
+        
+        let result = main.ligneToutesEnBas(4);
+
+        expect(result).toBe(4);
+    })
+    /*it("doit return 2 quand je donne 2", function(){
         let result = main.ligneToutesEnBas(dateMinute);
 
         expect(result).toBe("Lampe 1 et 2 en bas jaune allumé");
-    });
+    });*/
     
 })
