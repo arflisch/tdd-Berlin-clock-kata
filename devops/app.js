56ac7edb0e061c959a13519d746c65414fbbb627
arnaud-flisch-vinci <arnaud.flisch@student.vinci.be>
2023-10-12 18:09:54 +0200

    app modified

--- devops/app.js
+++ devops/app.js
@@ -3,12 +3,19 @@ export class Main{
         let nombreMin
         if (number % 5 === 0){
             nombreMin = number/5;
+            return "Aucune lampe allumé";
         }else{
             if ( number % 5 ===1){
                 return "Lampe 1 en bas jaune allumé";
             };
             if (number % 5 === 2){
-                return "lampe 2 en bas jaune allumé";
+                return "Lampe 1 et 2 en bas jaune allumé";
+            }
+            if (number % 5 === 3){
+                return "Lampe 1, 2 et 3 en bas jaune allumé";
+            }
+            if (number % 5 === 4){
+                return "Les 4 lampes en bas jaune allumé";
             }
         };
     }
--- devops/spec/appspec.js
+++ devops/spec/appspec.js
@@ -1,20 +1,19 @@
-import { Main } from "../app";
+import { Main } from "../app.js";
 
 describe("Ligne simple min", function() {
     let main = new Main();
-
+    let dateMinute = new Date().getMinutes;
     it("doit return 1 quand je donne 1", function(){
-        let dateMinute = new Date().getMinutes;
+        
 
-
-        let result = main.ligneToutesEnBaseline(dateMinute);
+        let result = main.ligneToutesEnBas(dateMinute);
 
         expect(result).toBe("Lampe 1 en bas jaune allumé");
     })
     it("doit return 2 quand je donne 2", function(){
         let result = main.ligneToutesEnBas(dateMinute);
 
-        expect(result).toBe("Lampe 2 en bas jaune allumé");
+        expect(result).toBe("Lampe 1 et 2 en bas jaune allumé");
     });
     
 })
